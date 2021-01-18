032697d6c672dbf5f67fb4ab8cc73c4a8b5ba281
Trey Davis <trey@clicktripz.com>
2021-01-18 13:09:46 -0800

    Some progress

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # Charlie
 
 A man-in-the-middle (MITM) HTTP/S proxy tool.
+
+
+
+## TODO: route all traffic through the proxy on macOS:
+sudo networksetup -setwebproxy Ethernet proxyserver.example.com 80 off
+networksetup -listallnetworkservices
+
+
+Setting up a proxy with networksetup: (check you available adapters with networksetup -listallnetworkservices before this)
+
+networksetup -setwebproxy "Wi-fi" 127.0.0.1 8080
+If required, you can setup authentication with the following syntax: [-setwebproxy networkservice domain portnumber authenticated username password]
+
+Turning the proxy on or off:
+
+networksetup -setwebproxystate "Wi-fi" off
+View the proxy status:
+
+networksetup -getwebproxy "Wi-Fi"
+
+
+## TODO: netcat in clojure/java
+
+
+## adding proxy to aleph
+https://github.com/ztellman/aleph/pull/352/files
+
+
+## mitm proxy using netty
+https://github.com/hsiafan/cute-proxy/blob/d774856f6caf1ad0d80b404cd07f1b60b69150be/src/main/java/net/dongliu/proxy/netty/handler/HttpProxyHandler.java
+
+
+## SOCKS Proxy
+
+
+
+## reify vs proxy vs deftype
+https://groups.google.com/forum/#!msg/clojure/pZFl8gj1lMs/qVfIjQ4jDDMJ
--- src/mitm_charlie/main.clj
+++ src/mitm_charlie/main.clj
@@ -2,19 +2,34 @@
   (:import
    [io.netty.bootstrap ServerBootstrap Bootstrap]
    [io.netty.channel.socket.nio NioServerSocketChannel NioSocketChannel]
-   [io.netty.channel ChannelHandlerContext ChannelInboundHandlerAdapter
+   [io.netty.channel ChannelHandlerContext ChannelInboundHandlerAdapter ChannelDuplexHandler
     ChannelInitializer ChannelOption ChannelHandler ChannelFutureListener
     ChannelOutboundHandlerAdapter]
    [io.netty.channel.nio NioEventLoopGroup]
    [io.netty.handler.codec ByteToMessageDecoder]
    [io.netty.handler.logging LoggingHandler LogLevel]
+   [io.netty.handler.codec.http
+    DefaultFullHttpResponse
+    HttpContent HttpHeaders HttpUtil
+    HttpContentCompressor
+    HttpRequest HttpResponse
+    HttpResponseStatus DefaultHttpHeaders
+    HttpServerCodec HttpVersion HttpMethod
+    LastHttpContent HttpServerExpectContinueHandler
+    HttpHeaderNames]
    [io.netty.buffer Unpooled]
    [java.nio ByteBuffer ByteOrder]
    [java.io ByteArrayOutputStream]
    [io.netty.handler.codec.bytes ByteArrayEncoder]
    [java.util.concurrent LinkedBlockingQueue Executors]
    [java.util ArrayList]
-   [java.time Instant]))
+   [java.time Instant])
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
+
+
+
 
 (defn init-server-bootstrap
   [group handlers-factory]
@@ -48,11 +63,6 @@
   (->
    (.writeAndFlush channel Unpooled/EMPTY_BUFFER)
    (.addListener ChannelFutureListener/CLOSE)))
-
-(proxy [ChannelFutureListener] []
-  (operationComplete [complete-future]
-    (.. complete-future channel close)))
-
 
 (defn client-proxy-handler
   [source-channel]
@@ -90,42 +100,180 @@
       (connect target-host target-port)))
 
 
-(defn proxy-handler
-  [target-host target-port]
-  (let [outgoing-channel (atom nil)]
-    (proxy [ChannelInboundHandlerAdapter] []
-      (channelActive [ctx]
-        (->
-         (connect-client (.. ctx channel) target-host target-port)
-         (.addListener
-          (proxy [ChannelFutureListener] []
-            (operationComplete [complete-future]
-              (if (.isSuccess complete-future)
-                (do
-                  (reset! outgoing-channel (.channel complete-future))
-                  (.. ctx channel read))
-                (.close (.. ctx channel))))))))
-      (channelRead [ctx msg]
-        (->
-         (.writeAndFlush @outgoing-channel msg)
-         (.addListener
-          (proxy [ChannelFutureListener] []
-            (operationComplete [complete-future]
-              (if (.isSuccess complete-future)
-                (.. ctx channel read)
-                (flush-and-close (.. ctx channel))))))))
-      (channelInactive [ctx]
-        (when @outgoing-channel
-          (flush-and-close @outgoing-channel))))))
+(defn my-proxy-handler [readhandler]
+  (proxy [ChannelInboundHandlerAdapter] []
+    (channelRead [ctx msg]
+      (if (instance? HttpRequest msg)
+        (tap> [(.uri msg)])))))
+
+
+;; (spit "event.log" "test 1\n" :append true)
+(defn my-log [msg ctx]
+  (spit "event.log" (str/join " " [(.id (.channel ctx)) msg "\n"]) :append true))
+
+(defn my-logging-handler []
+  (proxy [ChannelDuplexHandler] []
+    (channelActive [ctx]
+      (my-log "channelActive" ctx)
+      (.. ctx channel read))
+    (channelRead [ctx msg]
+      (my-log "channelRead" ctx)
+      (.fireChannelRead ctx msg))
+    (channelUnregistered [ctx]
+      (my-log "channelUnregistered" ctx)
+      (.fireChannelUnregistered ctx))
+    (channelInactive [ctx]
+      (my-log "channelInactive" ctx)
+      (.fireChannelInactive ctx))
+    (channelRegistered [ctx]
+      (my-log "channelRegistered" ctx)
+      (.fireChannelRegistered ctx))
+    (exceptionCaught [ctx cause]
+      (my-log "exceptionCaught" ctx)
+      (tap> cause)
+      (.fireExceptionCaught ctx))
+    (userEventTriggered [ctx]
+      (my-log "userEventTriggered" ctx)
+      (.fireUserEventTriggered ctx))
+    (bind [ctx address prom]
+      (my-log "bind" ctx)
+      (.bind ctx address prom))
+    (connect [ctx remote-address local-address prom]
+      (my-log "connect" ctx)
+      (.connect ctx remote-address local-address prom))
+    (disconnect [ctx prom]
+      (my-log "disconnect" ctx)
+      (.disconnect ctx prom))
+    (close [ctx prom]
+      (my-log "close" ctx)
+      (.close ctx prom))
+    (deregister [ctx prom]
+      (my-log "deregister" ctx)
+      (.deregister ctx prom))
+    (channelReadComplete [ctx]
+      (my-log "channelReadComplete" ctx)
+      (.fireChannelReadComplete ctx))
+    (write [ctx msg prom]
+      (my-log "write" ctx)
+      (.write ctx prom))
+    (channelWritabilityChanged [ctx]
+      (my-log "channelWritabilityChanged" ctx)
+      (.fireChannelWritabilityChanged ctx))
+    (flush [ctx msg]
+      (my-log "flush" ctx)
+      (.flush ctx))
+    ))
+
+
 
 (comment
+  )
+
+
+
+(defn netty-request->ring-request [^HttpRequest req ssl?]
+  (let [question-mark-index (-> req .uri (.indexOf (int 63)))]
+    { :uri (let [idx (long question-mark-index)]
+             (if (neg? idx)
+               (.getUri req)
+               (.substring (.getUri req) 0 idx)))
+     :query-string (let [uri (.uri req)]
+                     (if (neg? question-mark-index)
+                       nil
+                       (.substring uri (unchecked-inc question-mark-index))))
+     :headers (.headers req) ;; turn this into a clojure map
+     :request-method (-> req .method .name str/lower-case keyword)
+     ;; :body body
+     :scheme (if ssl? :https :http)}))
+
+(comment
+  (defonce log (atom []))
+  (defn tap> [i]
+    (swap! log conj i))
+
+  (swap! log (fn [s] []))
+
   (defonce servers (atom []))
-  (.close (last @servers))
+  (do
+    (doseq [s @servers] (.close s))
+    (swap! servers (fn [s] [])))
+
   (swap! servers conj (start-server (fn []
                                       [
-                                       (LoggingHandler. "proxy" LogLevel/INFO)
-                                       (proxy-handler "www.freekpaans.nl" 80)
+                                       (my-logging-handler)
                                        ] )))
-  (doseq [s @servers]
-    (.close s))
+
+
+  (update-proxy my-logging-handler {"exceptionCaught" (fn [_ ctx cause] (.fireExceptionCaught ctx))})
+
+
+  (def msg nil)
+  (defn readit [ctx m]
+    (tap> m)
+    (println {:httprequest (instance? java.net.http.HttpRequest m)
+              })
+    )
+
+  (instance? java.net.http.HttpObject nil)
+
+
+  (let [s (java.net.Socket. "localhost" 9007 true)]
+    (with-open [w (io/writer s)]
+      (.write w "GET /hello-clj.txt HTTP/1.0\r\nHost: www.freekpaans.nl\r\n\r\n")
+      (.flush w)))
+
+  (def s (java.net.Socket. "localhost" 9007 true))
+  (.isClosed s)
+  ;; (.setTcpNoDelay s true) should be handled
+
+  (with-open [w (io/writer s)]
+    (.write w "GET /hello-clj.txt HTTP/1.0\r\nHost: www.freekpaans.nl\r\n\r\n")
+    (.flush w))
+  (with-open [r (io/reader s)
+              sw (java.io.StringWriter.)]
+    (io/copy r sw))
+
+
+
+
+
+  log
+
+  (def r (first @log))
+  ;; get host, port, authority
+  (.method r)
+  (.protocolVersion r)
+  (.get (.headers r) "Host")
+  (first (.headers r))
+  (doto
+      (io.netty.handler.codec.http.DefaultHttpHeaders.)
+    (.set "Boop" (into-array Long [1 2 3])))
+  (first ^Iterator (.iterator (.headers r)))
+  (java.net.URL. (.uri r))
+
+  (.channelInactive (my-logging-handler) (proxy [io.netty.channel.ChannelHandlerContext] [] (fireChannelActive [])))
+
+
+  (.getAllAppenders (io.netty.util.internal.logging.InternalLoggerFactory/getInstance "beep"))
+
+  (org.apache.logging.log4j.LogManager/getLogger "beep")
+  (.logger (LoggingHandler.))
+
+  (org.apache.log4j.Category.)
+
+  (.info  (io.netty.util.internal.logging.Log4JLogger. "boop") "abc")
+
+
+
+  (io.netty.util.internal.logging.InternalLoggerFactory/setDefaultFactory
+   io.netty.util.internal.logging.JdkLoggerFactory/INSTANCE)
+
+  (map + [1 2 3] [3 2 1])
+
+  (org.slf4j.LoggerFactory/getLogger "abc")
+
+
+
+
+
   )
